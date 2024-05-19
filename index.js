8dc68322cf00fc47cb924219b49cbb549e62bdd2
Andrea Bernicchia <abernicchia@salesforce.com>
2024-05-19 13:14:45 +0200

    absolute paths (logo/stylesheet) to relative paths + WEBAPP_VIRTUAL_ROOT for CDN/reverse proxy

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const path = require('path')
 
 const PORT = process.env.PORT || 5001
+const WEBAPP_VIRTUAL_ROOT = process.env.WEBAPP_VIRTUAL_ROOT || '/'
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(WEBAPP_VIRTUAL_ROOT, express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get(WEBAPP_VIRTUAL_ROOT, (req, res) => res.render('pages/index'))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
